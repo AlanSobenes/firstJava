37dce721513ded03d16789875d6929360bc56ce5
AlanSobenes <alansobenes@gmail.com>
2021-11-29 14:38:46 -0800

    commit

---
+++ MyOwn.java
@@ -0,0 +1,12 @@
+public class MyOwn {
+    public static void main(String[] args){
+        String name = "Alan";
+        Integer age = 35;
+        String homeTown = "Claremont, Ca";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + homeTown);
+
+    }
+}
